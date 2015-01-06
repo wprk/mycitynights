7155842f9c486b33fa06172407ff37f4634a7998
Will Parker <Will.Parker@widerplan.com>
2015-01-06 16:59:55 +0000

    Updated homepage so sections collapse in correct order

--- views/home.php
+++ views/home.php
@@ -1,7 +1,7 @@
 	<?php $this->load->view('featured_event', $featured_event); ?>
-	<div class="col-sm-12 col-md-4">
-		<h2>Upcoming Events:</h2>
-	</div>
-	<div class="col-sm-12 col-md-8">
+	<div class="col-sm-12 col-md-6 pull-right">
 		<h2>A little about us:</h2>
+	</div>
+	<div class="col-sm-12 col-md-6">
+		<h2>Upcoming Events:</h2>
 	</div>
